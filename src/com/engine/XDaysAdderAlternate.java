28c876559bebda2449d47410fa11ed8f72057ff2
jmarokhovsky <jmarokhovsky@gmail.com>
2016-12-16 19:59:55 -0500

    Create Alternate Adder
    Creating an alternate adder to take the interpretation of every stage in
    "The 12 Days of Christmas" as a list of all items received that day
    (i.e. the end of the second day results in the subject having 4 items,
    the partridge in the pear tree from the first day, one from the second
    day, and the 2 french hens).

---
+++ src/com/engine/XDaysAdderAlternate.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.engine;
+
+/**
+ * @author jmaro
+ *
+ */
+public class XDaysAdderAlternate implements IXDaysAdder {
+
+	/* (non-Javadoc)
+	 * @see com.engine.IXDaysAdder#getTotalItems(int)
+	 */
+	@Override
+	public int getTotalItems(int days) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ test/com/engine/XDaysAdderAlternateTest.java
@@ -0,0 +1,14 @@
+package com.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class XDaysAdderAlternateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
