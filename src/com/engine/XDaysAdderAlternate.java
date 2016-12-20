0c6e9b03a48663b4ee5e8e0fdf2724cac78ea3a1
jmarokhovsky <jmarokhovsky@gmail.com>
2016-12-20 17:13:25 -0500

    Implemented the Alternate Adder for all 12 days.

--- src/com/engine/XDaysAdderAlternate.java
+++ src/com/engine/XDaysAdderAlternate.java
@@ -11,6 +11,21 @@ import java.io.PrintStream;
  */
 public class XDaysAdderAlternate implements IXDaysAdder {
 	private int days;
+	private final String[] item_strs =
+	{
+			"partridge in a pear tree",
+			"turtle doves",
+			"french hens",
+			"calling birds",
+			"gold rings",
+			"geese a-laying",
+			"swans a-swimming",
+			"maids a-milking",
+			"ladies dancing",
+			"lords a-leaping",
+			"pipers piping",
+			"drummers drumming"
+	};
 
 	/* (non-Javadoc)
 	 * @see com.engine.IXDaysAdder#getTotalItems(int)
@@ -34,12 +49,26 @@ public class XDaysAdderAlternate implements IXDaysAdder {
 	@Override
 	public void printItems(PrintStream ps) {
 		String outputList = "No days were supplied.";
-		if (days > 0) {
-			outputList = "Day: 1\tItems: 1 partridge in a pear tree\n";
-		}
-		if (days > 1) {
-			String secondDay = "Day: 2\tItems: 2 turtle doves and 1 partridge in a pear tree\n";
-			outputList = outputList.concat(secondDay);
+		String dayStr = "Day: ";
+		String dISepStr = "\t";
+		String itemStr = "Items: ";
+		String endStr = "\n";
+		if (days > 0 && days <= 12) {
+			outputList = "";
+			for (int i = 0; i < days; i++) {
+				int currDay = i + 1;
+				String currItems = "";
+				for (int j = currDay; j > 0; j--) {
+					currItems += j + " " + item_strs[j - 1];
+					if (j > 1) {
+						currItems += ", ";
+					}
+				}
+				String currStr = dayStr + currDay + dISepStr + itemStr + currItems + endStr;
+				outputList += currStr;
+			}
+		} else if (days > 12) {
+			outputList = "The amount of days given are outside of the scope of the current printer.";
 		}
 		ps.print(outputList);
 	}
